638c3a2f41bb626e9f03d5baa70917876a32f0dc
Twometer <twometer@outlook.de>
2022-04-10 23:23:30 +0200

    Experimenting

--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
 import json
+import cv2
+import numpy as np
+
 islands = json.load(open("../islands/index.json"))
+contours = json.load(open("../islands/contours.json"))
+
+for contour in contours:
+    image = np.zeros((1000, 1000, 1), np.uint8)
+    for pt in contours[contour]:
+        cv2.circle(image, pt, 1, 255)
+    cv2.imshow(contour, image)
+    cv2.waitKey()
+    cv2.destroyWindow(contour)
+
+
 
 print(islands)
--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 import cv2
 import json
 import numpy as np
@@ -6,7 +8,6 @@ import numpy as np
 # Convert regular HSV to OpenCV HSV
 def hsv(h, s, v):
     arr = np.asarray([h / 2, s / 100 * 255, v / 100 * 255]).astype('uint8')
-    print(arr)
     return arr
 
 
@@ -22,6 +23,7 @@ def color_correct(img):
 
 
 islands = json.load(open("../islands/index.json"))
+island_contours = {}
 
 # image = cv2.imread('../images/train/d63ac231f44ab5c5.jpg')  # Reference
 # image = cv2.imread('../images/train/2b6bd63b9d574e0d.jpg') # Yellow test
@@ -41,22 +43,37 @@ def img_range(img, center, radius):
     return cv2.inRange(img, center - radius, center + radius)
 
 
+def normalize_contour(contour, shape):
+    points = []
+    for inpoints in contour:
+        for point in inpoints:
+            x = point[0] / shape[0] * 1000
+            y = point[1] / shape[1] * 1000
+            points.append((round(x), round(y)))
+    return points
+
+
 for i in islands:
     image = cv2.imread(f'../images/train/{i}.jpg')
 
-    image = cv2.bilateralFilter(image, 25, 80, 80)
+    image = cv2.bilateralFilter(image, 15, 50, 50)
     image = color_correct(image)
 
     image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    seaMask = cv2.inRange(image_hsv, hsv(65, 0, 35), hsv(265, 40, 100))
-    landMask = cv2.bitwise_not(seaMask)
+    seaMask1 = cv2.inRange(image_hsv, hsv(120, 0, 30), hsv(265, 40, 100))
+    seaMask2 = cv2.inRange(image_hsv, hsv(12, 0, 50), hsv(320, 15, 100))
+    landMask = cv2.bitwise_not(cv2.bitwise_or(seaMask1, seaMask2
+                                              ))
 
     kernel = cv2.getStructuringElement(shape=cv2.MORPH_ELLIPSE, ksize=(4, 4))
-    landMask = cv2.morphologyEx(landMask, cv2.MORPH_CLOSE, kernel, iterations=4)
+    landMask = cv2.morphologyEx(landMask, cv2.MORPH_CLOSE, kernel, iterations=3)
 
     contours, hierarchy = cv2.findContours(image=landMask, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(image=image, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2)
-    cv2.imshow('Island', image)
+    for idx in range(0, len(contours)):
+        area = cv2.contourArea(contours[idx])
+        if sqrt(area) > min(seaMask1.shape[0] / 10, 20):
+            cv2.drawContours(image=image, contours=contours, contourIdx=idx, color=(0, 255, 0), thickness=1)
+            island_contours[i] = normalize_contour(contours[idx], seaMask1.shape)
 
-    cv2.waitKey()
+json.dump(island_contours, open("../islands/contours.json", "w"))
